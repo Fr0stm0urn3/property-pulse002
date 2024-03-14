e8aa6b57294877df739a044ef155f9150dfe74bf
Cifer0101 <l.kakabadze2020@gmail.com>
2024-03-15 02:07:16 +0400

    Home Properties Component. Custom Not Found & Loading Page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -5,11 +5,15 @@ const PropertiesPage = () => {
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {properties.map((property) => (
-            <PropertyCard key={property.id} property={property} />
-          ))}
-        </div>
+        {properties.length === 0 ? (
+          <p>No Properties Found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   )
