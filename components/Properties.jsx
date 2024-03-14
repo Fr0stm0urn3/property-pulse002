6133d4ab841501020d03ef09ea12f4fefcd73b7b
Cifer0101 <l.kakabadze2020@gmail.com>
2024-03-15 01:07:15 +0400

    Properties Page

--- components/Properties.jsx
+++ components/Properties.jsx
@@ -1,13 +1,13 @@
 import properties from "@/properties.json"
-import PropertyCard from "@/components/PropertyCard"
+import Property from "./Property"
 
-const PropertiesPage = () => {
+const Properties = () => {
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {properties.map((property) => (
-            <PropertyCard key={property.id} property={property} />
+            <Property key={property.id} property={property} />
           ))}
         </div>
       </div>
@@ -15,4 +15,4 @@ const PropertiesPage = () => {
   )
 }
 
-export default PropertiesPage
+export default Properties
