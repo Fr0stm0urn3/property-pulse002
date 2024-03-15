c84814810f55e05e4437f12e5aab5616425bad86
Cifer0101 <l.kakabadze2020@gmail.com>
2024-03-15 21:45:41 +0400

    Add Property POST API Route

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -101,7 +101,7 @@ const PropertyAddForm = () => {
 
   return (
     isMounted && (
-      <form>
+      <form action="/api/properties" method="POST" encType="multipart/form-data">
         <h2 className="text-3xl text-center font-semibold mb-6">Add Property</h2>
 
         <div className="mb-4">
@@ -529,6 +529,7 @@ const PropertyAddForm = () => {
             accept="image/*"
             multiple
             onChange={handleImageChange}
+            required
           />
         </div>
 
