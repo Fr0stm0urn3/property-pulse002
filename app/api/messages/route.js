acf1924fe001bb006893e9764eaf741a31ca9049
Cifer0101 <l.kakabadze2020@gmail.com>
2024-03-18 18:47:08 +0400

    Message Submit & API Route

---
+++ app/api/messages/route.js
@@ -0,0 +1,47 @@
+import connectDB from "@/config/database"
+import { getSessionUser } from "@/utils/getSessionUser"
+import Message from "@/models/Message"
+
+export const dynamic = "force-dynamic"
+
+//POST /api/messages
+export const POST = async (request) => {
+  try {
+    await connectDB()
+
+    const { email, phone, name, message, recipient, property } = await request.json()
+
+    const sessionUser = await getSessionUser()
+
+    if (!sessionUser || !sessionUser.user) {
+      return new Response("User ID is required", { status: 401 })
+    }
+
+    const { user } = sessionUser
+
+    //Can not send message to self
+    if (user.id === recipient) {
+      return new Response(
+        JSON.stringify({ message: "You must be logged in to send a message" }),
+        { status: 401 }
+      )
+    }
+
+    const newMessage = new Message({
+      sender: user.id,
+      recipient,
+      property,
+      name,
+      email,
+      phone,
+      body: message,
+    })
+
+    await newMessage.save()
+
+    return new Response(JSON.stringify({ message: "Message Sent" }), { status: 200 })
+  } catch (error) {
+    console.log(error)
+    return new Response("Something went wrong", { status: 500 })
+  }
+}
