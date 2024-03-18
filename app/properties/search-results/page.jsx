b42943c6221cee60d0dab34bded85372c52ee0d9
Cifer0101 <l.kakabadze2020@gmail.com>
2024-03-18 06:08:29 +0400

    Display Search Results

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -2,9 +2,11 @@
 
 import { useState, useEffect } from "react"
 import Spinner from "@/components/Spinner"
-import { toast } from "react-toastify"
 import { useSearchParams } from "next/navigation"
 import PropertyCard from "@/components/PropertyCard"
+import Link from "next/link"
+import { FaArrowAltCircleLeft } from "react-icons/fa"
+import PropertySearchForm from "@/components/PropertySearchForm"
 
 const SearchResultsPage = () => {
   const searchParams = useSearchParams()
@@ -12,8 +14,6 @@ const SearchResultsPage = () => {
   const [properties, setProperties] = useState([])
   const [loading, setLoading] = useState(true)
 
-  console.log(searchParams.get("location"))
-  console.log(searchParams.get("propertyType"))
   const location = searchParams.get("location")
   const propertyType = searchParams.get("propertyType")
 
@@ -39,24 +39,38 @@ const SearchResultsPage = () => {
     fetchSearchResults()
   }, [location, propertyType])
 
-  if (loading) {
-    return <Spinner loading={loading} />
-  }
-
   return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
-          <p className="text-center font-bold text-2xl">No Properties Found</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+      {loading ? (
+        <Spinner loading={loading} />
+      ) : (
+        <section className="px-4 py-6">
+          <div className="container-xl lg:container m-auto px-4 py-6">
+            <Link
+              className="flex items-center text-blue-500 hover:underline mb-3"
+              href={"/properties"}
+            >
+              <FaArrowAltCircleLeft className="mr-2" /> Back To Properties
+            </Link>
+            <h1 className="text-2xl font-bold text-center mb-4">Search Results</h1>
+            {properties.length === 0 ? (
+              <p className="text-center font-bold text-2xl">No Search Results Found.</p>
+            ) : (
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6">
+                {properties.map((property) => (
+                  <PropertyCard key={property._id} property={property} />
+                ))}
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </section>
+        </section>
+      )}
+    </>
   )
 }
 
