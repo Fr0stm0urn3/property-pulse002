6c5ff5bb4fc60a36f59526f5f07f17b5a47bddcc
Cifer0101 <l.kakabadze2020@gmail.com>
2024-03-23 08:25:01 +0400

    Display Search Results + Implement search Form in pages

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -4,6 +4,9 @@ import { useState, useEffect } from "react"
 import Spinner from "@/components/Spinner"
 import { useSearchParams } from "next/navigation"
 import PropertyCard from "@/components/PropertyCard"
+import Link from "next/link"
+import { FaArrowAltCircleLeft } from "react-icons/fa"
+import PropertySearchForm from "@/components/PropertySearchForm"
 
 const SearchResultsPage = () => {
   const searchParams = useSearchParams()
@@ -32,12 +35,40 @@ const SearchResultsPage = () => {
     fetchSearchedProperties()
   }, [location, propertyType])
 
-  return loading ? (
-    <Spinner loading={loading} />
-  ) : properties.length === 0 ? (
-    <h3 className="font-bold text-2xl mt-40 mx-auto">No Properties Found.</h3>
-  ) : (
-    properties.map((property) => <PropertyCard key={property._id} property={property} />)
+  return (
+    <>
+      <section className="bg-gray-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+      {loading ? (
+        <Spinner loading={loading} />
+      ) : (
+        <section className="px-4 py-6">
+          <div className="container-xl lg:container m-auto px-4 py-6">
+            <Link
+              href="/properties"
+              className="flex items-center hover:text-gray-500 hover:underline mb-4"
+            >
+              <FaArrowAltCircleLeft className="mb-0.5 mr-2" /> Back To Properties
+            </Link>
+            <h1 className="text-2xl font-bold mb-4 text-center">Search Results</h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.length === 0 ? (
+                <p className="font-bold text-xl mt-10 mx-auto">
+                  No search results found.
+                </p>
+              ) : (
+                properties.map((property) => (
+                  <PropertyCard key={property._id} property={property} />
+                ))
+              )}
+            </div>
+          </div>
+        </section>
+      )}
+    </>
   )
 }
 
