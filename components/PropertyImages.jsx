83ed43a7e86e4e40611d413c93759698d14183cf
Cifer0101 <l.kakabadze2020@gmail.com>
2024-03-24 12:58:46 +0400

    Photoswipe Lightbox Gallery

--- components/PropertyImages.jsx
+++ components/PropertyImages.jsx
@@ -1,43 +1,58 @@
 import Image from "next/image"
+import { Gallery, Item } from "react-photoswipe-gallery"
 
 const PropertyImages = ({ images }) => {
   return (
-    <section className="bg-blue-50 p-4">
-      <div className="container mx-auto">
-        {images.length === 1 ? (
-          <Image
-            src={images[0]}
-            alt=""
-            className="object-cover h-[400px] mx-auto rounded-xl"
-            width={0}
-            height={0}
-            priority={true}
-            sizes="100vw"
-          />
-        ) : (
-          <div className="grid grid-cols-2 gap-4">
-            {images.map((image, i) => (
-              <div
-                key={i}
-                className={`${
-                  images.length === 3 && i === 2 ? "col-span-2" : "col-span-1"
-                }`}
-              >
+    <Gallery>
+      <section className="bg-blue-50 p-4">
+        <div className="container mx-auto">
+          {images.length === 1 ? (
+            <Item original={images[0]} thumbnail={images[0]} height="600" width="1000">
+              {({ ref, open }) => (
                 <Image
-                  src={image}
+                  src={images[0]}
                   alt=""
-                  className="object-cover h-[400px] w-full rounded-xl"
+                  className="object-cover h-[400px] mx-auto rounded-xl"
                   width={0}
                   height={0}
-                  sizes="100vw"
                   priority={true}
+                  sizes="100vw"
+                  ref={ref}
+                  onClick={open}
                 />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+              )}
+            </Item>
+          ) : (
+            <div className="grid grid-cols-2 gap-4">
+              {images.map((image, i) => (
+                <div
+                  key={i}
+                  className={`${
+                    images.length === 3 && i === 2 ? "col-span-2" : "col-span-1"
+                  }`}
+                >
+                  <Item original={image} thumbnail={image} height="600" width="1000">
+                    {({ ref, open }) => (
+                      <Image
+                        src={image}
+                        alt=""
+                        className="object-cover h-[400px] w-full rounded-xl"
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        priority={true}
+                        ref={ref}
+                        onClick={open}
+                      />
+                    )}
+                  </Item>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </Gallery>
   )
 }
 
